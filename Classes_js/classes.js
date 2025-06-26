720da0e6b003c129adf0b5369982491a72f77486
Kaua Peres <kauaperessdure13@gmail.com>
2025-06-26 19:57:23 -0300

    atividades corrigidas

--- Classes_js/classes.js
+++ Classes_js/classes.js
@@ -37,6 +37,13 @@ class Estoque {
   adicionarProduto(produto) {
     this.produtos.push(produto);
   }
+
+  listarProdutos() {
+    console.log("Produtos no estoque:");
+    this.produtos.forEach((p) => {
+      console.log(p.nome + " - R$" + p.preco);
+    });
+  }
 }
 
 class Carrinho {
@@ -47,12 +54,34 @@ class Carrinho {
   adicionarItem(produto, quantidade) {
     this.itens.push({ produto, quantidade });
   }
+
+  calcularTotal() {
+    let total = 0;
+    this.itens.forEach((item) => {
+      total += item.produto.preco * item.quantidade;
+    });
+    return total;
+  }
+
+  listarItens() {
+    console.log("Itens no carrinho:");
+    this.itens.forEach((item) => {
+      console.log(item.quantidade + "x " + item.produto.nome);
+    });
+  }
 }
 
 class Pedido {
   constructor(cliente, carrinho) {
     this.cliente = cliente;
     this.carrinho = carrinho;
+    this.total = carrinho.calcularTotal();
+  }
+
+  mostrarResumo() {
+    console.log("Cliente: " + this.cliente.nome);
+    this.carrinho.listarItens();
+    console.log("Total: R$" + this.total);
   }
 }
 
@@ -61,6 +90,10 @@ class Pagamento {
     this.valor = valor;
     this.metodo = metodo;
   }
+
+  confirmarPagamento() {
+    console.log("Pagamento de R$" + this.valor + " feito com " + this.metodo);
+  }
 }
 
 class Supermercado {
@@ -68,3 +101,26 @@ class Supermercado {
     this.nome = nome;
   }
 }
+
+// Simulação com dados
+
+const mercado = new Supermercado("Mercado Bom");
+
+const produto1 = new Produto("Arroz", 20);
+const produto2 = new Produto("Feijão", 8);
+
+const estoque = new Estoque();
+estoque.adicionarProduto(produto1);
+estoque.adicionarProduto(produto2);
+estoque.listarProdutos();
+
+const cliente = new Cliente("João");
+const carrinho = new Carrinho();
+carrinho.adicionarItem(produto1, 2);
+carrinho.adicionarItem(produto2, 1);
+
+const pedido = new Pedido(cliente, carrinho);
+pedido.mostrarResumo();
+
+const pagamento = new Pagamento(pedido.total, "Cartão");
+pagamento.confirmarPagamento();
